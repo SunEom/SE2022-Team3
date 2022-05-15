fbf690c60ae53ae80321ece53afbf21ecc470c2b
Suneom <dkxl0701@gmail.com>
2022-05-15 12:39:23 +0900

    로그인 화면 기본 UI를 작성하였습니다.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import App from "./App";
+import Login from "./Screens/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
