3db51bea085462304e3ac478fce8c45eacc2b756
Suneom <dkxl0701@gmail.com>
2022-05-23 14:36:11 +0900

    코드에 대한 comment를 수정하였습니다.

--- frontend/src/Presenter/Screens/Main/components/Like/LikeContainer.jsx
+++ frontend/src/Presenter/Screens/Main/components/Like/LikeContainer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import LikePresenter from "./LikePresenter";
 
 const LikeContainer = ({ clothList, page, setMaxPage }) => {
-  // 특정 계절에 맞는 의상 리스트
+  // 좋아요를 누른 의상 리스트
   const filteredClothList = clothList.filter((c) => c.favorite);
 
-  // 특정 계절에 맞는 의상 중 현재 페이지에서 보여지고 있는 의상 리스트
+  // 좋아요를 누른 의상 중 현재 페이지에서 보여지고 있는 의상 리스트
   const nowPageClothList = filteredClothList.slice((page - 1) * 10, page * 10);
 
   // 마지막 페이지 번호 설정
