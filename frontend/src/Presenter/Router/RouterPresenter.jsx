efc80b508bb3eb6cb8efc05dce6b230ab604469f
Suneom <dkxl0701@gmail.com>
2022-05-23 14:24:25 +0900

    로그인 여부를 확인하는 Router 컴포넌트를 추가하였습니다.

---
+++ frontend/src/Presenter/Router/RouterPresenter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Main from "../Screens/Main";
+import Login from "../Screens/Login";
+import Header from "../Components/Header";
+import Join from "../Screens/Join";
+import MyPage from "../Screens/MyPage";
+import Board from "../Screens/Board";
+import PostDetail from "../Screens/PostDetail";
+
+const RouterPresenter = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/join" element={<Join />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
+        <Route path="/board/:category" element={<Board />}></Route>
+        <Route path="/postdetail/:post_id" element={<PostDetail />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RouterPresenter;
