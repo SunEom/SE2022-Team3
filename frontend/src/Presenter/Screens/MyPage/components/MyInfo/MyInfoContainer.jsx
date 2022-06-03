94278ba05b58e5fc34c638c886853bdd54d273b0
SunEom <dkxl0701@gmail.com>
2022-06-03 23:03:35 +0900

    나의 정보 화면을 업데이트 하였습니다.

--- frontend/src/Presenter/Screens/MyPage/components/MyInfo/MyInfoContainer.jsx
+++ frontend/src/Presenter/Screens/MyPage/components/MyInfo/MyInfoContainer.jsx
@@ -1,32 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { fetchMyData } from "../../../../../httpRequest";
 import MyInfoPresenter from "./MyInfoPresenter";
 
 const MyInfoContainer = () => {
-  const [userInfo, setUserInfo] = useState({
-    nickname: window.localStorage.getItem("nickname"),
-    age: 25,
-    gender: "비공개",
-  }); // 사용자의 정보
+  const [loading, setLoading] = useState(true);
   const [rows, setRows] = useState([]); // 테이블에 표시되도록 사용자의 정보를 가공한 배열
 
-  const fetchUserInfo = async () => {
-    // 서버에 사용자 정보 요청 (수정 예정)
-    // await axios
-    //   .post(`${process.env.REACT_APP_SERVER_URL}/user`, { idToken: window.localStorage.getItem("idToken") })
-    //   .then((response) => setUserInfo(response.data.user))
-    //   .catch((err) => console.error(err));
-  };
-
   useEffect(() => {
-    fetchUserInfo();
-    setRows([
-      { key: "닉네임", value: userInfo.nickname },
-      { key: "성별", value: userInfo.gender },
-      { key: "나이", value: userInfo.age },
-    ]);
+    fetchMyData().then((result) => {
+      setRows([
+        { key: "닉네임", value: result.nickname },
+        { key: "성별", value: result.gender },
+        { key: "나이", value: result.age },
+      ]);
+      setLoading(false);
+    });
   }, []);
 
-  return <MyInfoPresenter rows={rows} />;
+  return <MyInfoPresenter rows={rows} loading={loading} />;
 };
 
 export default MyInfoContainer;
--- frontend/src/httpRequest.js
+++ frontend/src/httpRequest.js
@@ -578,12 +578,98 @@ export const requestDeleteUserData = async () => {
   axiosPostRequest("/user/signout");
 };
 
+const tempPostings = [
+  { id: 1, title: "나만 알고 있는 옷 보관 꿀팁1", comments: 10, nickname: "suneom" },
+  { id: 2, title: "나만 알고 있는 옷 보관 꿀팁2", comments: 7, nickname: "qkrco" },
+  { id: 3, title: "나만 알고 있는 옷 보관 꿀팁3", comments: 15, nickname: "mihee" },
+  { id: 4, title: "나만 알고 있는 옷 보관 꿀팁4", comments: 20, nickname: "ashrain" },
+  { id: 5, title: "나만 알고 있는 옷 보관 꿀팁5", comments: 11, nickname: "kimcw" },
+  { id: 6, title: "나만 알고 있는 옷 보관 꿀팁6", comments: 10, nickname: "suneom" },
+  { id: 7, title: "나만 알고 있는 옷 보관 꿀팁7", comments: 7, nickname: "qkrco" },
+  { id: 8, title: "나만 알고 있는 옷 보관 꿀팁8", comments: 15, nickname: "mihee" },
+  { id: 9, title: "나만 알고 있는 옷 보관 꿀팁9", comments: 20, nickname: "ashrain" },
+  { id: 10, title: "나만 알고 있는 옷 보관 꿀팁10", comments: 11, nickname: "kimcw" },
+  { id: 11, title: "나만 알고 있는 옷 보관 꿀팁11", comments: 10, nickname: "suneom" },
+  { id: 12, title: "나만 알고 있는 옷 보관 꿀팁12", comments: 7, nickname: "qkrco" },
+  { id: 13, title: "나만 알고 있는 옷 보관 꿀팁13", comments: 15, nickname: "mihee" },
+  { id: 14, title: "나만 알고 있는 옷 보관 꿀팁14", comments: 20, nickname: "ashrain" },
+  { id: 15, title: "나만 알고 있는 옷 보관 꿀팁15", comments: 11, nickname: "kimcw" },
+  { id: 16, title: "나만 알고 있는 옷 보관 꿀팁16", comments: 10, nickname: "suneom" },
+  { id: 17, title: "나만 알고 있는 옷 보관 꿀팁17", comments: 7, nickname: "qkrco" },
+  { id: 18, title: "나만 알고 있는 옷 보관 꿀팁18", comments: 15, nickname: "mihee" },
+  { id: 19, title: "나만 알고 있는 옷 보관 꿀팁19", comments: 20, nickname: "ashrain" },
+  { id: 20, title: "나만 알고 있는 옷 보관 꿀팁20", comments: 11, nickname: "kimcw" },
+  { id: 21, title: "나만 알고 있는 옷 보관 꿀팁21", comments: 10, nickname: "suneom" },
+  { id: 22, title: "나만 알고 있는 옷 보관 꿀팁22", comments: 7, nickname: "qkrco" },
+  { id: 23, title: "나만 알고 있는 옷 보관 꿀팁23", comments: 15, nickname: "mihee" },
+  { id: 24, title: "나만 알고 있는 옷 보관 꿀팁24", comments: 20, nickname: "ashrain" },
+  { id: 25, title: "나만 알고 있는 옷 보관 꿀팁25", comments: 11, nickname: "kimcw" },
+  { id: 26, title: "나만 알고 있는 옷 보관 꿀팁26", comments: 10, nickname: "suneom" },
+  { id: 27, title: "나만 알고 있는 옷 보관 꿀팁27", comments: 7, nickname: "qkrco" },
+  { id: 28, title: "나만 알고 있는 옷 보관 꿀팁28", comments: 15, nickname: "mihee" },
+  { id: 29, title: "나만 알고 있는 옷 보관 꿀팁29", comments: 20, nickname: "ashrain" },
+  { id: 30, title: "나만 알고 있는 옷 보관 꿀팁30", comments: 11, nickname: "kimcw" },
+  { id: 31, title: "나만 알고 있는 옷 보관 꿀팁31", comments: 10, nickname: "suneom" },
+  { id: 32, title: "나만 알고 있는 옷 보관 꿀팁32", comments: 7, nickname: "qkrco" },
+  { id: 33, title: "나만 알고 있는 옷 보관 꿀팁33", comments: 15, nickname: "mihee" },
+  { id: 34, title: "나만 알고 있는 옷 보관 꿀팁34", comments: 20, nickname: "ashrain" },
+  { id: 35, title: "나만 알고 있는 옷 보관 꿀팁35", comments: 11, nickname: "kimcw" },
+  { id: 36, title: "나만 알고 있는 옷 보관 꿀팁36", comments: 10, nickname: "suneom" },
+  { id: 37, title: "나만 알고 있는 옷 보관 꿀팁37", comments: 7, nickname: "qkrco" },
+  { id: 38, title: "나만 알고 있는 옷 보관 꿀팁38", comments: 15, nickname: "mihee" },
+  { id: 39, title: "나만 알고 있는 옷 보관 꿀팁39", comments: 20, nickname: "ashrain" },
+  { id: 40, title: "나만 알고 있는 옷 보관 꿀팁40", comments: 11, nickname: "kimcw" },
+  { id: 41, title: "이렇게 입어보면 어떨까요1", comments: 10, nickname: "suneom" },
+  { id: 42, title: "이렇게 입어보면 어떨까요2", comments: 7, nickname: "qkrco" },
+  { id: 43, title: "이렇게 입어보면 어떨까요3", comments: 15, nickname: "mihee" },
+  { id: 44, title: "이렇게 입어보면 어떨까요4", comments: 20, nickname: "ashrain" },
+  { id: 45, title: "이렇게 입어보면 어떨까요5", comments: 11, nickname: "kimcw" },
+  { id: 46, title: "이렇게 입어보면 어떨까요6", comments: 10, nickname: "suneom" },
+  { id: 47, title: "이렇게 입어보면 어떨까요7", comments: 7, nickname: "qkrco" },
+  { id: 48, title: "이렇게 입어보면 어떨까요8", comments: 15, nickname: "mihee" },
+  { id: 49, title: "이렇게 입어보면 어떨까요9", comments: 20, nickname: "ashrain" },
+  { id: 50, title: "이렇게 입어보면 어떨까요10", comments: 11, nickname: "kimcw" },
+  { id: 51, title: "이렇게 입어보면 어떨까요11", comments: 10, nickname: "suneom" },
+  { id: 52, title: "이렇게 입어보면 어떨까요12", comments: 7, nickname: "qkrco" },
+  { id: 53, title: "이렇게 입어보면 어떨까요13", comments: 15, nickname: "mihee" },
+  { id: 54, title: "이렇게 입어보면 어떨까요14", comments: 20, nickname: "ashrain" },
+  { id: 55, title: "이렇게 입어보면 어떨까요15", comments: 11, nickname: "kimcw" },
+  { id: 56, title: "이렇게 입어보면 어떨까요16", comments: 10, nickname: "suneom" },
+  { id: 57, title: "이렇게 입어보면 어떨까요17", comments: 7, nickname: "qkrco" },
+  { id: 58, title: "이렇게 입어보면 어떨까요18", comments: 15, nickname: "mihee" },
+  { id: 59, title: "이렇게 입어보면 어떨까요19", comments: 20, nickname: "ashrain" },
+  { id: 60, title: "이렇게 입어보면 어떨까요20", comments: 11, nickname: "kimcw" },
+  { id: 61, title: "이렇게 입어보면 어떨까요21", comments: 10, nickname: "suneom" },
+  { id: 62, title: "이렇게 입어보면 어떨까요22", comments: 7, nickname: "qkrco" },
+  { id: 63, title: "이렇게 입어보면 어떨까요23", comments: 15, nickname: "mihee" },
+  { id: 64, title: "이렇게 입어보면 어떨까요24", comments: 20, nickname: "ashrain" },
+  { id: 65, title: "이렇게 입어보면 어떨까요25", comments: 11, nickname: "kimcw" },
+  { id: 66, title: "이렇게 입어보면 어떨까요26", comments: 10, nickname: "suneom" },
+  { id: 67, title: "이렇게 입어보면 어떨까요27", comments: 7, nickname: "qkrco" },
+  { id: 68, title: "이렇게 입어보면 어떨까요28", comments: 15, nickname: "mihee" },
+  { id: 69, title: "이렇게 입어보면 어떨까요29", comments: 20, nickname: "ashrain" },
+  { id: 70, title: "이렇게 입어보면 어떨까요30", comments: 11, nickname: "kimcw" },
+  { id: 71, title: "이렇게 입어보면 어떨까요31", comments: 10, nickname: "suneom" },
+  { id: 72, title: "이렇게 입어보면 어떨까요32", comments: 7, nickname: "qkrco" },
+  { id: 73, title: "이렇게 입어보면 어떨까요33", comments: 15, nickname: "mihee" },
+  { id: 74, title: "이렇게 입어보면 어떨까요34", comments: 20, nickname: "ashrain" },
+  { id: 75, title: "이렇게 입어보면 어떨까요35", comments: 11, nickname: "kimcw" },
+  { id: 76, title: "이렇게 입어보면 어떨까요36", comments: 10, nickname: "suneom" },
+  { id: 77, title: "이렇게 입어보면 어떨까요37", comments: 7, nickname: "qkrco" },
+  { id: 78, title: "이렇게 입어보면 어떨까요38", comments: 15, nickname: "mihee" },
+  { id: 79, title: "이렇게 입어보면 어떨까요39", comments: 20, nickname: "ashrain" },
+  { id: 80, title: "이렇게 입어보면 어떨까요40", comments: 11, nickname: "kimcw" },
+];
+
 // 본인이 작성한 글 목록 조회
 export const fetchMyPosting = async () => {
-  axiosPostRequest("/mypage/post");
+  return tempPostings.filter((p) => p.nickname === "suneom");
+
+  // axiosPostRequest("/mypage/post");
 };
 
 // 본인이 좋아요를 누른 글 목록 조회
 export const fetchMyFavPosting = async () => {
-  axiosPostRequest("/mypage/favorite_post");
+  return tempPostings.filter((p) => p.nickname === "ashrain");
+  // axiosPostRequest("/mypage/favorite_post");
 };
