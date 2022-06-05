c7b871d37bb4aa3c719e80664cb6dcbccc4ed81a
AshRainK <kjo980430@gmail.com>
2022-06-05 21:11:59 +0900

    마이페이지 기능 구현 완료

--- backend/src/main/java/closet/backend/controller/MyPageController.java
+++ backend/src/main/java/closet/backend/controller/MyPageController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -21,13 +22,15 @@ public class MyPageController {
     private final MyPageService myPageService;
 
     @PostMapping("/mypage/post")
-    public ResponseEntity getPosts(@RequestBody String idToken) throws FirebaseAuthException {
+    public ResponseEntity getPosts(@RequestBody Map<String, String> req) throws FirebaseAuthException {
+        String idToken = req.get("idToken");
         List<PostDtoWithCommentCount> result = myPageService.getPosts(idToken);
         return ResponseEntity.ok().body(result);
     }
 
     @PostMapping("/mypage/favorite_post")
-    public ResponseEntity getFavoritePost(@RequestBody String idToken) throws FirebaseAuthException{
+    public ResponseEntity getFavoritePost(@RequestBody Map<String, String> req) throws FirebaseAuthException{
+        String idToken = req.get("idToken");
         List<PostDtoWithCommentCount> result = myPageService.getFavoritePost(idToken);
         return ResponseEntity.ok().body(result);
     }
