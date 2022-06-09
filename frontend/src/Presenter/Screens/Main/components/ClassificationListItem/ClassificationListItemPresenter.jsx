e473b6b5c754badf24ef00f4371a153caeca136f
SunEom <dkxl0701@gmail.com>
2022-06-09 18:32:47 +0900

    의상 상세 화면을 수정하였습니다.

--- frontend/src/Presenter/Screens/Main/components/ClassificationListItem/ClassificationListItemPresenter.jsx
+++ frontend/src/Presenter/Screens/Main/components/ClassificationListItem/ClassificationListItemPresenter.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { Button, IconButton, MenuItem, TextField } from "@mui/material";
+import { MenuItem } from "@mui/material";
 import styled from "styled-components";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import SaveIcon from "@mui/icons-material/Save";
-import CancelIcon from "@mui/icons-material/Cancel";
 
 const ItemContainer = styled.div`
   display: flex;
@@ -12,43 +8,13 @@ const ItemContainer = styled.div`
   justify-content: space-between;
 `;
 
-const ClassificationListItemPresenter = ({
-  classification,
-  onChange,
-  onModeToggleButtonClick,
-  onSaveButtonClick,
-  mode,
-  editedName,
-  onDeleteButtonClick,
-  onClassificationClick,
-}) => {
+const ClassificationListItemPresenter = ({ classification, onClassificationClick }) => {
   return (
-    <>
-      {mode === "show" && (
-        <ItemContainer>
-          <MenuItem style={{ width: "100%" }} onClick={onClassificationClick}>
-            {classification.folder_name}
-          </MenuItem>
-          <IconButton size="small" color="primary" onClick={onModeToggleButtonClick}>
-            <EditIcon />
-          </IconButton>
-          <IconButton size="small" color="error" onClick={onDeleteButtonClick}>
-            <DeleteIcon />
-          </IconButton>
-        </ItemContainer>
-      )}
-      {mode === "edit" && (
-        <ItemContainer>
-          <TextField size="small" value={editedName} onChange={onChange}></TextField>
-          <IconButton size="small" color="primary" onClick={onSaveButtonClick}>
-            <SaveIcon />
-          </IconButton>
-          <IconButton size="small" color="error" onClick={onModeToggleButtonClick}>
-            <CancelIcon />
-          </IconButton>
-        </ItemContainer>
-      )}
-    </>
+    <ItemContainer>
+      <MenuItem style={{ width: "100%" }} onClick={onClassificationClick}>
+        {classification.folder_name}
+      </MenuItem>
+    </ItemContainer>
   );
 };
 
